f20828030e86cfa27134e6673fc5b7da95ea77d7
fedilayoub <ayoubzahi2016@gmail.com>
2021-10-14 18:15:08 +0100

    Updates

---
+++ precache-manifest.c6d1899ccb19777a17d471157823b4d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc31a3bda3580f3425846f03027e049",
+    "url": "/tours-descovery-react/index.html"
+  },
+  {
+    "revision": "7e91daea779671c2c2de",
+    "url": "/tours-descovery-react/static/css/main.b4b424f9.chunk.css"
+  },
+  {
+    "revision": "e26865ee9065fd180b8b",
+    "url": "/tours-descovery-react/static/js/2.35913d98.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tours-descovery-react/static/js/2.35913d98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e91daea779671c2c2de",
+    "url": "/tours-descovery-react/static/js/main.6a2668fe.chunk.js"
+  },
+  {
+    "revision": "cea26e3177d47bba51cc",
+    "url": "/tours-descovery-react/static/js/runtime-main.3b323d4c.js"
+  }
+]);
